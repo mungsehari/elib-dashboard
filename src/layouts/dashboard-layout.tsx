eddaa5e6a784cbee7b1472642b4e4d436760005c
mungsehari <mungse.hari@gmail.com>
2024-07-29 17:56:55 +0530

    add logout functionality

--- src/layouts/dashboard-layout.tsx
+++ src/layouts/dashboard-layout.tsx
@@ -33,10 +33,16 @@ import {
 import useTokenStore from "../store";
 
 const DashboardLayout = () => {
-  const token = useTokenStore((store) => store.token);
+  const { token, setToken } = useTokenStore((store) => store);
+
   if (!token) {
     return <Navigate to={"/auth/login"} replace />;
   }
+
+  const logout = () => {
+    console.log("logging out");
+    setToken("");
+  };
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
@@ -192,7 +198,11 @@ const DashboardLayout = () => {
               <DropdownMenuItem>Settings</DropdownMenuItem>
               <DropdownMenuItem>Support</DropdownMenuItem>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
+              <DropdownMenuItem>
+                <Button onClick={logout} variant={"link"}>
+                  Logout
+                </Button>
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
