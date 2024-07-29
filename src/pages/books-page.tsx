d5fc0fe151d884be446a1711084abe388ee5e727
mungsehari <mungse.hari@gmail.com>
2024-07-29 19:01:43 +0530

    show books table

--- src/pages/books-page.tsx
+++ src/pages/books-page.tsx
@@ -1,7 +1,155 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBooks } from "../http/api";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "../components/ui/breadcrumb";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../components/ui/table";
+import { Badge } from "../components/ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "../components/ui/dropdown-menu";
+import { Button } from "../components/ui/button";
+import { LoaderCircle, MoreHorizontal } from "lucide-react";
+import { Book } from "../types";
+
 const BooksPages = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["books"],
+    queryFn: getBooks,
+    staleTime: 10000,
+  });
+  console.log("data", data);
+
+  if (isError) return <div>Error :</div>;
+
   return (
     <div>
-      <p>Book</p>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/dashboard/home">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Books</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <Card className="mt-6">
+        <CardHeader>
+          <CardTitle>Books</CardTitle>
+          <CardDescription>
+            Manage your books and view their sales performance.
+            {isLoading && (
+              <span className="inline-block w-[20px] h-[20px] mx-auto animate-spin">
+                <LoaderCircle />
+              </span>
+            )}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="hidden w-[100px] sm:table-cell">
+                  <span className="sr-only">Image</span>
+                </TableHead>
+                <TableHead>Title</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead className="hidden md:table-cell">Price</TableHead>
+                <TableHead className="hidden md:table-cell">
+                  Author name
+                </TableHead>
+                <TableHead className="hidden md:table-cell">
+                  Created at
+                </TableHead>
+                <TableHead>
+                  <span className="sr-only">Actions</span>
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {data?.data.map((book: Book) => {
+                return (
+                  <TableRow key={book.id}>
+                    <TableCell className="hidden sm:table-cell">
+                      <img
+                        alt={book.title}
+                        className="aspect-square rounded-md object-cover"
+                        height="64"
+                        src={book.imageUrl}
+                        width="64"
+                      />
+                    </TableCell>
+                    <TableCell className="font-medium">{book.title}</TableCell>
+                    <TableCell>
+                      <Badge variant="outline">Fiction</Badge>
+                    </TableCell>
+                    <TableCell className="hidden md:table-cell">
+                      ₹{book.price}
+                    </TableCell>
+                    <TableCell className="hidden md:table-cell">
+                      {book.author}
+                    </TableCell>
+                    <TableCell className="hidden md:table-cell">
+                      {book.createdAt}
+                    </TableCell>
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button
+                            aria-haspopup="true"
+                            size="icon"
+                            variant="ghost"
+                          >
+                            <MoreHorizontal className="h-4 w-4" />
+                            <span className="sr-only">Toggle menu</span>
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                          <DropdownMenuItem>Edit</DropdownMenuItem>
+                          <DropdownMenuItem>Delete</DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </CardContent>
+        <CardFooter>
+          <div className="text-xs text-muted-foreground">
+            Showing <strong>1-10</strong> of <strong>32</strong> products
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+
+export interface Book{
+    id:string;
+    title:string;
+    author:string;
+    description:string;
+    imageUrl:string;
+    price:number;
+    createdAt:string
+}
