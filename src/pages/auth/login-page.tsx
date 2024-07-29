979ef33ef6b6bc3a28dbd1b6444b6799019c6ef8
mungsehari <mungse.hari@gmail.com>
2024-07-29 16:33:07 +0530

    presist access token in local storege

--- src/pages/auth/login-page.tsx
+++ src/pages/auth/login-page.tsx
@@ -14,17 +14,20 @@ import { useRef } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { login } from "../../http/api";
 import { LoaderCircle } from "lucide-react";
+import useTokenStore from "../../store";
 
 const LoginPage = () => {
   const navigate = useNavigate();
+  const setToken = useTokenStore((state) => state.setToken);
+
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
   const mutation = useMutation({
     mutationFn: login,
 
-    onSuccess: () => {
-      console.log("login successfully");
+    onSuccess: (response) => {
+      setToken(response.data.jwt);
       navigate("/dashboard/home");
     },
   });
